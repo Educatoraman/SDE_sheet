1f9f17d2e73592ddce99ed8796a3e46c9b09f365
Aman Singh <128976778+Educatoraman@users.noreply.github.com>
2024-05-08 17:08:43 +0530

    Approach 1

---
+++ SortColors.java
@@ -0,0 +1,9 @@
+// Approach 1
+// T.C.--> O(N logN) , S.C. --> O(1)
+//BRUTE FORCE --> Directly inbuilt sort method ka use krke SORT kardo
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+
+    }
+}
